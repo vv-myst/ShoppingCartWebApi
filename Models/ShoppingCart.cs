296c77d55d543f9653ef114f5d64fa6049646388
vv-myst <vivek.vardhan86@gmail.com>
2018-02-18 20:16:19 +0100

    removed unwanted files

--- Models/ShoppingCart.cs
+++ Models/ShoppingCart.cs
@@ -5,6 +5,14 @@ namespace ShoppingCartWebApi.Models
 {
     public class ShoppingCart
     {
+        public ShoppingCart()
+        {
+            ItemList = new List<Item>();
+            ItemCountMap = new Dictionary<int, int>();
+            ItemCount = 0;
+            TotalValue = 0.00M;
+        }
+
         public ShoppingCart(IList<Item> itemList)
         {
             ItemList = itemList;
@@ -33,42 +41,13 @@ namespace ShoppingCartWebApi.Models
         /// </summary>
         public decimal TotalValue { get; internal set; }
 
-        /// <summary>
-        ///     Updates the total quantity of each item in the ItemCountMap dictionary
-        ///     when a new item is added to the shopping cart
-        /// </summary>
-        public void UpdateItemCountMapUponAdd()
+        private void UpdateItemCountMapUponAdd()
         {
             foreach (var item in ItemList)
                 ItemCountMap[item.Id] = ItemList.Count(x => x.Id == item.Id);
         }
 
-        /// <summary>
-        ///     Updates the total quantity of each item in the ItemCountMap dictionary
-        ///     when items of a particular id are deleted from the shopping cart.
-        /// </summary>
-        /// <param name="itemId"></param>
-        public void UpdateItemCountMapUponDelete(int itemId)
-        {
-            if (ItemList.Count == 0)
-                ClearItemCountMap();
-            else
-                ItemCountMap[itemId] = 0;
-        }
-
-        /// <summary>
-        ///     Clears all the entries in the ItemCountMap dictionary
-        /// </summary>
-        public void ClearItemCountMap()
-        {
-            ItemCountMap.Clear();
-        }
-
-        /// <summary>
-        ///     Updates the remaining properties of the shopping cart when an item
-        ///     is either added or deleted from the cart.
-        /// </summary>
-        public void UpdateShoppingCart()
+        private void UpdateShoppingCart()
         {
             ItemCount = ItemList.Count;
             TotalValue = ItemList.Sum(item => item.Value);
